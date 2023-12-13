80fec453582cd0b099ce1080b7816c56098417de
ajanedy <andy.janedy@gmail.com>
2023-12-12 19:33:08 -0500

    Final commit with working unit tests, completed shell_script, completed docstrings

--- shell_script.sh
+++ shell_script.sh
@@ -1,8 +1,15 @@
-pytest test_functions.py
-git add .
-echo "enter a commit message:"
-read commit_comment
-git commit -m "$commit_comment"
-echo "what branch?"
-read branch
-git push origin "$branch"
+pytest -v
+pytest_status=$?
+pytest_pass=0
+if [ $pytest_status -eq $pytest_pass ]
+then
+  git add .
+  echo "enter a commit message:"
+  read commit_comment
+  git commit -m "$commit_comment"
+  echo "what branch?"
+  read branch
+  git push origin "$branch"
+else
+  echo "at least one unit test did not pass"
+fi
